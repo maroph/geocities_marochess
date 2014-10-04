430ce79c1bf471f7a7bbf5daf466335850849745
Manfred Rosenboom <maroph@gmail.com>
2014-10-04 11:56:23 +0200

    update first assignment data

--- w3c_training/rwd/assignment1/bnb/bnb.js
+++ w3c_training/rwd/assignment1/bnb/bnb.js
@@ -1,7 +1,7 @@
 /*
  * Author      : Manfred Rosenboom
  * Site        : http://www.geocities.ws/marochess/w3c_training/rwd/assignment1/
- * Last Update : Fri, 5 Sep 2014 14:31:00 +0200
+ * Last Update : Fri, 3 Oct 2014 16:47:00 +0200
  * License     : Creative Commons Attribution 4.0 International
  *               http://creativecommons.org/licenses/by/4.0/
  * =============================================================================
@@ -12,10 +12,6 @@ function popw(pagename, windowname) {
 
 function redir(uri) {
     window.open(uri)
-}
-
-function showAdvertisement() {
-    window.open('./promo.html','Advertisement',config='height=120, width=450, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no, fullscreen=yes');
 }
 
 function closeWindow() {
